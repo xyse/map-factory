48bab32fcf39d6ad4e2a1085d03f83b352a9d70e
Juko, Nico <nico.juko@ap-solut.com>
2024-02-02 15:17:26 +0100

    Revert "fix: try smth else"
    This reverts commit 75b2b9d910168d36ce07f8747cf746fdd139f3a8.

--- src/lib/mapper.js
+++ src/lib/mapper.js
@@ -290,13 +290,13 @@ export default class Mapper {
 
     let values = [];
     let anyValues = false;
-    
+
     // Get source
     for (const fromKey of sourcePath) {
 
       const value = this.om.getValue(sourceObject, fromKey);
 
-      if (this.exists_(value)) {
+      if (value !== undefined) {
         anyValues = true;
       }
 
@@ -306,7 +306,7 @@ export default class Mapper {
     let value;
 
     // default transformations
-    if ((anyValues || options.alwaysTransform) === true && options.pipelineTransformations.length > 0) {
+    if (anyValues && options.pipelineTransformations.length > 0) {
       options.pipelineTransformations.map(item => {
         values = item(sourceObject, values);
       });
@@ -316,7 +316,12 @@ export default class Mapper {
     for (let i = 0; i < values.length; i++) {
       values[i] = this.flattenValue_(flattenings[i], values[i]);
     }
-    
+
+    // Apply transform if appropriate
+    if (anyValues || options.alwaysTransform === true) {
+      value = transform(...values);
+    }
+
     if (!anyValues && failureTransform) {
       value = failureTransform(value);
     }
